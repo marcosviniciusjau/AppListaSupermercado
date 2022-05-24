646712fa321e85d25fbbe5a4b18b49652f4e2dc0
Usuario <Usuario@CPD03-M18>
2022-05-24 12:26:19 -0300

    inicio do banco de dados

--- AppListaSupermercado/AppListaSupermercado/Helper/SQLiteDatabaseHelper.cs
+++ AppListaSupermercado/AppListaSupermercado/Helper/SQLiteDatabaseHelper.cs
@@ -4,29 +4,48 @@ using System.Text;
 
 using SQLite;
 using AppListaSupermercado.Model;
-
+using System.Threading.Tasks;
 namespace AppListaSupermercado.Helper
 {
     public class SQLiteDatabaseHelper
     {
-        public void insert(Produto p)
-        {
+        readonly SQLiteAsyncConnection _connection;
 
+        public SQLiteDatabaseHelper(string path)
+        {
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> save(Produto p)
+        {
+            return _connection.InsertAsync(p);
         }
 
         public void getAllRows()
         {
-
+            return _connection.Table<Produto>().ToListAsync();
         }
 
-        public void delete(int id)
+        public Task<int> delete(int id)
         {
-
+            return _connection.Table<Produto>().DeleteAsync(i => i.Id == id);
         }
 
-        public void update(Produto p)
+        public Task<List<Produto>> Update (Produto p)
         {
+            string sql = "UPDATE produto SET " +
+                                    "NomeProduto=?, Quantidade=?, PrecoEstimado=?, PrecoPago=? " +
+                                    "WHERE Id=?";
 
+            return _connection.QueryAsync<Produto>(sql,
+                p.NomeProduto, p.Quantidade, p.PrecoEstimado, p.PrecoPago, p.Id);
+        }
+
+        public Task<List<Produto> Search(string q)
+        {
+            string sql = "SELECT * FROM produto WHERE Descricao LIKE '%" + q + "%'";
+
+            return _connection.QueryAsync<Produto>(sql);
         }
     }
 }
--- AppListaSupermercado/AppListaSupermercado/View/ListaProdutos.xaml.cs
+++ AppListaSupermercado/AppListaSupermercado/View/ListaProdutos.xaml.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using AppListaSupermercado.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Xaml
 
 namespace AppListaSupermercado.View
 {
@@ -16,6 +18,84 @@ namespace AppListaSupermercado.View
         {
             InitializeComponent();
         }
+        protected override void OnAppearing()
+        {
+            ObservableCollection<Produto> tarefas = new ObservableCollection<Produto>();
 
+            System.Threading.Tasks.Task.Run(async () =>
+            {
+                List<Produto> temp = await App.Database.GetAllRows();
+
+                foreach (Produto item in temp)
+                {
+                    tarefas.Add(item);
+                }
+
+                atualizando.IsRefreshing = false;
+            });
+
+            lista.ItemsSource = tarefas;
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            ObservableCollection<Produto> tarefas = new ObservableCollection<Produto>();
+
+            MenuItem disparador = (MenuItem)sender;
+
+            Produto produto_selecionado = (Produto)disparador.BindingContext;
+
+            bool confirmacao = await DisplayAlert("Tem ctza?", "Remover o Produto?", "Sim", "Não");
+
+            if (confirmacao)
+            {
+                await System.Threading.Tasks.Task.Run(async () =>
+                {
+                    await App.Database.Delete(produto_selecionado.Id);
+
+                    List<Produto> temp = await App.Database.GetAllRows();
+
+                    foreach (Produto item in temp)
+                    {
+                        tarefas.Add(item);
+                    }
+
+                    atualizando.IsRefreshing = false;
+                });
+
+                lista.ItemsSource = tarefas;
+            }
+        }
+
+        private void lista_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Produto produto_selecionado = (Produto)e.SelectedItem;
+
+            Navigation.PushAsync(new AbrirProduto
+            {
+                BindingContext = produto_selecionado
+            });
+        }
+
+        private void txt_busca_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ObservableCollection<Produto> tarefas = new ObservableCollection<Produto>();
+
+            string q = e.NewTextValue;
+
+            Task.Run(async () =>
+            {
+                List<Produto> temp = await App.Database.Search(q);
+
+                foreach (Produto item in temp)
+                {
+                    tarefas.Add(item);
+                }
+
+                atualizando.IsRefreshing = false;
+            });
+
+            lista.ItemsSource = produtos;
+        }
     }
 }
